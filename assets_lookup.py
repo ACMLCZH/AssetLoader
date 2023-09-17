a6eb66b6725b857f53e747794c50fa11d900af63
ACMLCZH <965503066@qq.com>
2023-09-18 04:06:08 +0800

    Add glb asset

--- assets_lookup.py
+++ assets_lookup.py
@@ -10,15 +10,14 @@ class TextureSource:
         texture,
         roughness,
         normal,
+        metallic = None,
         displace = None
     ):
         self.texture = os.path.join(texture_folder, texture)
         self.roughness = os.path.join(texture_folder, roughness)
         self.normal = os.path.join(texture_folder, normal)
-        if displace is None:
-            self.displace = None
-        else:
-            self.displace = os.path.join(texture_folder, displace)
+        self.metallic = None if metallic is None else os.path.join(texture_folder, metallic)
+        self.displace = None if displace is None else os.path.join(texture_folder, displace)
 
 class BackgroundSource:
     def __init__(
@@ -26,6 +25,24 @@ class BackgroundSource:
         texture,
     ):
         self.texture = os.path.join(background_folder, texture)
+
+class ModelSource:
+    def __init__(
+        self,
+        object,
+        texture,
+        roughness,
+        normal,
+        metallic = None,
+        displace = None
+    ):
+        self.object = os.path.join(model_folder, object)
+        self.texture = os.path.join(model_folder, texture)
+        self.roughness = os.path.join(model_folder, roughness)
+        self.normal = os.path.join(model_folder, normal)
+        self.metallic = None if metallic is None else os.path.join(model_folder, metallic)
+        self.displace = None if displace is None else os.path.join(model_folder, displace)
+
 
 texture_lookup = {
     "dark_wood": TextureSource(
@@ -65,4 +82,14 @@ texture_lookup = {
 background_lookup = {
     "sunflowers_puresky": BackgroundSource("sunflowers_puresky_8k.hdr"),
     "brown_photostudio_07": BackgroundSource("brown_photostudio_07_8k.hdr"), 
+}
+
+model_look_up = {
+    "wooden_table": ModelSource(
+        object="blend/wooden_table/models/wooden_table_02.obj",
+        texture="blend/wooden_table/textures/wooden_table_02_diff_4k.jpg",
+        roughness="blend/wooden_table/textures/wooden_table_02_rough_4k.jpg",
+        normal="blend/wooden_table/textures/wooden_table_02_nor_gl_4k.exr",
+        metallic="blend/wooden_table/textures/wooden_table_02_metal_4k.exr",
+    ),
 }
--- process_glb.py
+++ process_glb.py
@@ -5,9 +5,7 @@ import trimesh
 from PIL import Image
 from io import BytesIO
 import pygltflib
-
-from . import geom as geom_utils
-import unisim as us
+import argparse
 
 class GLBObject:
     def __init__(self,
@@ -16,23 +14,31 @@ class GLBObject:
         vertex_normals,
         vertex_colors,
         uvs,
-        image_texture,
-        metallic_texture,
-        roughness_texture
+        material,
     ):
         self.vertices          = vertices
         self.faces             = faces
         self.vertex_normals    = vertex_normals
         self.vertex_colors     = vertex_colors
         self.uvs               = uvs
+        self.material          = material
+
+class GLBMaterial:
+    def __init__(self,
+        image_texture,
+        metallic_texture,
+        roughness_texture
+    ):
         self.image_texture     = image_texture
         self.metallic_texture  = metallic_texture
         self.roughness_texture = roughness_texture
 
 class GLBObjectScene:
-    def __init__(self, filename):
+    def __init__(self, filename, name=None):
         self.filename = filename
+        self.name = filename if name is None else name
         self.objects = list()
+        self.materials = list()
 
         glb = pygltflib.GLTF2().load(filename)
         glb.convert_images(pygltflib.ImageFormat.DATAURI)       # for accessing the binary image data
@@ -49,9 +55,9 @@ class GLBObjectScene:
             mesh = glb.meshes[mesh_list[i][0]]
             matrix = mesh_list[i][1]
             primitive = mesh.primitives[0]
-            us.logger.debug(f"Mesh index: {i}")
-            us.logger.debug(f"    Attributes={primitive.attributes}")
-            us.logger.debug(f"    Indices={primitive.indices}, Material={primitive.material}")
+            print(f"Mesh index: {i}")
+            print(f"    Attributes={primitive.attributes}")
+            print(f"    Indices={primitive.indices}, Material={primitive.material}")
             # "primitive.attributes" records the indices in "accessors". It is like:
             #      Attributes(POSITION=2, NORMAL=1, TANGENT=None, TEXCOORD_0=None, TEXCOORD_1=None,
             #                 COLOR_0=None, JOINTS_0=None, WEIGHTS_0=None)
@@ -65,8 +71,8 @@ class GLBObjectScene:
                     points_buffer_view.byteOffset + points_buffer_view.byteLength
                 ],
                 dtype=np.float32, count=points_accessor.count * 3,
-            ).reshape((-1, 3))
-            us.logger.debug(f"Points: {points.shape}")
+            ).reshape((-1, 3)).astype(float)
+            print(f"Points: {points.shape}")
             
             # parse faces
             triangles_accessor = glb.accessors[primitive.indices]
@@ -86,8 +92,8 @@ class GLBObjectScene:
                 ],
                 dtype=triangles_t, count=triangles_accessor.count
             ).reshape((-1, 3)).astype(np.int32)
-            us.logger.debug(f"Triangles: {triangles.shape}, "
-                            f"{triangles_buffer_view.byteLength - triangles_accessor.byteOffset}")
+            print(f"Triangles: {triangles.shape}, "
+                  f"{triangles_buffer_view.byteLength - triangles_accessor.byteOffset}")
 
             # parse normals
             if primitive.attributes.NORMAL:
@@ -99,57 +105,12 @@ class GLBObjectScene:
                         normals_buffer_view.byteOffset + normals_buffer_view.byteLength
                     ],
                     dtype=np.float32, count=normals_accessor.count * 3,
-                ).reshape((-1, 3))
-                us.logger.debug(f"Normals: {normals.shape}, "
-                                f"{normals_buffer_view.byteLength - normals_accessor.byteOffset}")
+                ).reshape((-1, 3)).astype(float)
+                print(f"Normals: {normals.shape}, "
+                      f"{normals_buffer_view.byteLength - normals_accessor.byteOffset}")
             else:
-                normals = None
+                normals = trimesh.Trimesh(points, triangles).vertex_normals
 
-            # parse images
-            color_image, metallic_image, roughness_image = None, None, None
-            if primitive.material is not None:
-                material = glb.materials[primitive.material]
-
-                # Check if material has a base color texture
-                if material.pbrMetallicRoughness.baseColorTexture is not None:
-                    texture = glb.textures[material.pbrMetallicRoughness.baseColorTexture.index]
-                    image_index = texture.source
-                    color_image = uri_to_image(glb.images[image_index].uri)
-                    # image_data = Image.open(image_data).convert("RGBA")
-
-                # parse colors
-                if material.pbrMetallicRoughness.baseColorFactor is not None:
-                    colors_factor = material.pbrMetallicRoughness.baseColorFactor
-                    colors_factor = np.array(colors_factor, dtype=np.float32)
-                    if color_image is None:
-                        color_image = Image.new("RGBA", (1, 1), (255, 255, 255, 255))
-                    # colors_factor
-                    bands = color_image.split()
-                    color_image = Image.merge("RGBA", [
-                        Image.eval(bands[i], lambda x: x * colors_factor[i]) for i in range(4)
-                    ])
-
-                if material.pbrMetallicRoughness.metallicRoughnessTexture is not None:
-                    texture = glb.textures[material.pbrMetallicRoughness.metallicRoughnessTexture.index]
-                    image_index = texture.source
-                    image = uri_to_image(glb.images[image_index].uri)
-                    # image_data = BytesIO()
-                    # image = Image.open(image_data)
-                    bands = image.split()
-                    metallic_image, roughness_image = bands[1], bands[2]    # G for metallic, B for roughness
-
-                if material.pbrMetallicRoughness.metallicFactor is not None:
-                    metallic_factor = material.pbrMetallicRoughness.metallicFactor
-                    if metallic_image is None:
-                        metallic_image = Image.new("L", (1, 1), 255)
-                    metallic_image = Image.eval(metallic_image, lambda x: x * metallic_factor)
-
-                if material.pbrMetallicRoughness.roughnessFactor is not None:
-                    roughness_factor = material.pbrMetallicRoughness.roughnessFactor
-                    if roughness_image is None:
-                        roughness_image = Image.new("L", (1, 1), 255)
-                    roughness_image = Image.eval(roughness_image, lambda x: x * roughness_factor)
-            
             # parse uvs
             if primitive.attributes.TEXCOORD_0:
                 texcoords_accessor = glb.accessors[primitive.attributes.TEXCOORD_0]
@@ -160,10 +121,11 @@ class GLBObjectScene:
                         texcoords_buffer_view.byteOffset + texcoords_buffer_view.byteLength
                     ],
                     dtype=np.float32, count=texcoords_accessor.count * 2,
-                ).reshape((-1, 2))
+                ).reshape((-1, 2)).astype(float)
+                uvs[:, 1] = 1.0 - uvs[:, 1]
             else:
                 uvs = None
-
+                
             # parse colors
             if primitive.attributes.COLOR_0:
                 colors_accessor = glb.accessors[primitive.attributes.COLOR_0]
@@ -189,10 +151,12 @@ class GLBObjectScene:
                 ).reshape((-1, colors_vec))
                 if colors_t == np.float32:
                     colors = np.round(colors * 255).astype(np.uint8)
-                us.logger.debug(f"Colors: {colors.shape}, "
-                                f"{colors_buffer_view.byteLength - colors_accessor.byteOffset}")
+                print(f"Colors: {colors.shape}, "
+                      f"{colors_buffer_view.byteLength - colors_accessor.byteOffset}")
             else:
                 colors = None
+            
+            material_index = primitive.material
 
             # rebuild mesh and save to .obj file
             points, normals = apply_transform(matrix, points, normals)
@@ -202,64 +166,123 @@ class GLBObjectScene:
                 vertex_normals=normals,
                 vertex_colors=colors,
                 uvs=uvs,
+                material=material_index
+            ))
+
+        for i in range(len(glb.materials)):
+            # parse images
+            color_image, metallic_image, roughness_image = None, None, None
+            material = glb.materials[i]
+
+            # Check if material has a base color texture
+            if material.pbrMetallicRoughness.baseColorTexture is not None:
+                texture = glb.textures[material.pbrMetallicRoughness.baseColorTexture.index]
+                image_index = texture.source
+                color_image = uri_to_image(glb.images[image_index].uri)
+                # image_data = Image.open(image_data).convert("RGBA")
+
+            # parse colors
+            if material.pbrMetallicRoughness.baseColorFactor is not None:
+                colors_factor = material.pbrMetallicRoughness.baseColorFactor
+                colors_factor = np.array(colors_factor, dtype=np.float32)
+                if color_image is None:
+                    color_image = Image.new("RGBA", (1, 1), (255, 255, 255, 255))
+                # colors_factor
+                bands = color_image.split()
+                color_image = Image.merge("RGBA", [
+                    Image.eval(bands[i], lambda x: x * colors_factor[i]) for i in range(4)
+                ])
+
+            if material.pbrMetallicRoughness.metallicRoughnessTexture is not None:
+                texture = glb.textures[material.pbrMetallicRoughness.metallicRoughnessTexture.index]
+                image_index = texture.source
+                image = uri_to_image(glb.images[image_index].uri)
+                bands = image.split()
+                if len(bands) == 1:
+                    roughness_image = bands[0]
+                else:
+                    metallic_image, roughness_image = bands[1], bands[2]    # G for metallic, B for roughness
+
+            if material.pbrMetallicRoughness.metallicFactor is not None:
+                metallic_factor = material.pbrMetallicRoughness.metallicFactor
+                if metallic_image is None:
+                    metallic_image = Image.new("L", (1, 1), 255)
+                metallic_image = Image.eval(metallic_image, lambda x: x * metallic_factor)
+
+            if material.pbrMetallicRoughness.roughnessFactor is not None:
+                roughness_factor = material.pbrMetallicRoughness.roughnessFactor
+                if roughness_image is None:
+                    roughness_image = Image.new("L", (1, 1), 255)
+                roughness_image = Image.eval(roughness_image, lambda x: x * roughness_factor)
+
+            self.materials.append(GLBMaterial(
                 image_texture=color_image,
                 metallic_texture=metallic_image,
                 roughness_texture=roughness_image
             ))
-            
+
     def obj_count(self):
         return len(self.objects)
 
     def get_object(self, index=0):
         return self.objects[index]
     
-    def export_object(self, index=0):
-        object_dir = self.filename + '.objects'
-        os.makedirs(object_dir, exist_ok=True)
+    def export_all(self):
+        export_dir = os.path.join(os.path.split(self.filename)[0], self.name)
+        model_dir = os.path.join(export_dir, "models")
+        texture_dir = os.path.join(export_dir, "textures")
+        os.makedirs(model_dir, exist_ok=True)
+        os.makedirs(texture_dir, exist_ok=True)
+        for i in range(len(self.objects)):
+            self.export_object(model_dir, i)
+        for i in range(len(self.materials)):
+            self.export_texture(texture_dir, i)
+
+    def export_object(self, dir_name, index=0):
+        # object_dir = self.filename + '.objects'
         obj = self.objects[index]
-        obj_file = os.path.join(object_dir, f"object_{index}.obj")
-        us.logger.info(f"Export mesh index {index} to: {obj_file}")
+        obj_file = os.path.join(dir_name, f"object_{index}.obj")
+        print(f"Export mesh index {index} to: {obj_file}")
         mesh = trimesh.Trimesh(
             vertices=obj.vertices,
             faces=obj.faces,
             vertex_normals=obj.vertex_normals,
             vertex_colors=obj.vertex_colors
         )
+        mesh.visual = trimesh.visual.texture.TextureVisuals(uv=obj.uvs)
+        export_without_mtl(mesh, obj_file)
 
-        # Metallic and roughness will be discarded
-        if obj.image_texture is not None:
-            us.logger.debug(f"Image Texture")
-            mesh.visual = trimesh.visual.texture.TextureVisuals(uv=obj.uvs, image=obj.image_texture)
-        else:
-            us.logger.debug(f"Color Texture")
-            mesh.visual.uv = obj.uvs
-            # visuals = trimesh.visual.color.ColorVisuals(mesh=mesh, vertex_colors=colors)
-            # visuals = visuals.to_texture()
-            # visuals.uv = texcoords
-            # mesh.visual = visuals
-        mesh.export(obj_file, file_type="obj", include_color=True)
+    def export_texture(self, dir_name, index=0):
+        mat = self.materials[index]
 
-    def export_texture(self, index=0):
-        image_dir = self.filename + '.textures'
-        os.makedirs(image_dir, exist_ok=True)
-        obj = self.objects[index]
-        if obj.image_texture is not None:
-            color_image_filename = os.path.join(image_dir, f'color_{index}.png')
-            obj.image_texture.save(color_image_filename)
-        else:
-            color_image_filename = None
+        if mat.image_texture is not None:
+            color_image_file = os.path.join(dir_name, f'color_{index}.png')
+            print(f"Export texture index {index} to: {color_image_file}")
+            mat.image_texture.save(color_image_file)
     
-        if obj.metallic_texture is not None:
-            metallic_image_filename = os.path.join(image_dir, f'matallic_{index}.png')
-            obj.metallic_texture.save(metallic_image_filename)
-        else:
-            metallic_image_filename = None
+        if mat.metallic_texture is not None:
+            metallic_image_file = os.path.join(dir_name, f'matallic_{index}.png')
+            print(f"Export texture index {index} to: {metallic_image_file}")
+            mat.metallic_texture.save(metallic_image_file)
     
-        if obj.roughness_texture is not None:
-            roughness_image_filename = os.path.join(image_dir, f'roughness_{index}.png')
-            obj.roughness_texture.save(roughness_image_filename)
-        else:
-            roughness_image_filename = None
+        if mat.roughness_texture is not None:
+            roughness_image_file = os.path.join(dir_name, f'roughness_{index}.png')
+            print(f"Export texture index {index} to: {roughness_image_file}")
+            mat.roughness_texture.save(roughness_image_file)
+
+
+def export_without_mtl(mesh, path: str):
+    obj_str = trimesh.exchange.obj.export_obj(
+        mesh, include_color=True, include_texture=True, write_texture=False
+    )
+    obj_list = obj_str.split("\n")
+    obj_list = [s for s in obj_list if not s.startswith("usemtl") and 
+                                       not s.startswith("mtllib") and 
+                                       not s.startswith("#")]
+    obj_str = "\n".join(obj_list)
+    fw = open(path, "w")
+    fw.write(obj_str)
+    fw.close()
 
 def uri_to_image(uri):
     image_data = base64.b64decode(uri.split(",")[1])
@@ -305,31 +328,29 @@ def parse_tree(nodes, node_index):
         mesh_list[i][1] = mesh_list[i][1] @ matrix
     return mesh_list
 
-def process_glb_aspose(glb_file):
-    from aspose3dcloud import Scene, SaveOptions, FileFormat
-    # Load GLB file via the from_file of Scene class
-    obj_file = get_obj_filename(glb_file)
-    scene = Scene.from_file(glb_file)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check_all_models", action="store_true", default=False)
+    parser.add_argument("--path", type=str, default=None)
+    parser.add_argument("--name", type=str, default=None)
+    args = parser.parse_args()
 
-    # Call the Scene.save method with OBJ’s format
-    scene.save(obj_file, SaveOptions(FileFormat.WAVEFRONTOBJ))
-
-def process_glb_trimesh(glb_file):
-    obj_file = get_obj_filename(glb_file)
-    scene = trimesh.load(glb_file)
-
-    trimesh.exchange.export.export_mesh(scene, obj_file)
-    
-def get_obj_filename(glb_file, suffix=""):
-    us.logger.debug(f"Converting file: {glb_file} {os.path.exists(glb_file)}")
-    filename, extension = os.path.splitext(glb_file)
-    # if not extension.lower().endswith('.glb'):
-    #     raise Exception("Not glb file")
-    obj_file = f"{filename}_{suffix}.obj"
-    return obj_file
-
-def get_png_pic(dir_name, file_name, suffix):
-    filename = os.path.split(file_name)[1]
-    prefix = os.path.splitext(filename)[0]
-    png_pic = os.path.join(dir_name, f"{prefix}_{suffix}.png")
-    return png_pic
+    # test_file = "./assets/models/glb/eraser.glb"
+    if args.check_all_models:
+        walks = os.walk("./assets/models/glb/")
+        for dir_path, dirnames, filenames in walks:
+            for filename in filenames:
+                if filename.endswith(".glb"):
+                    file_path = os.path.join(dir_path, filename)
+                    name = os.path.splitext(filename)[0]
+                    scene = GLBObjectScene(file_path, name)
+                    scene.export_all()
+    else:
+        file_path = args.path
+        if args.name is None:
+            filename = os.path.split(file_path)[1]
+            name = os.path.splitext(filename)[0]
+        else:
+            name = args.name
+        scene = GLBObjectScene(file_path, name)
+        scene.export_all()
