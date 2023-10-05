8d0605281746948688fb21867117c5253176dfbc
ACMLCZH <965503066@qq.com>
2023-10-06 00:42:27 +0800

    Update build_luisa_gif.py

--- build_luisa_gif.py
+++ build_luisa_gif.py
@@ -11,25 +11,31 @@ if __name__ == "__main__":
     parser.add_argument("--scripts_dir", type=str, default=None)
     parser.add_argument("--script_path", type=str, default=None)
     parser.add_argument("--script_mark", type=str, default=None)
-    parser.add_argument("--name", type=str, default=None)
     args = parser.parse_args()
 
     if args.scripts_dir is not None and args.script_mark is not None:
         files = os.listdir(args.scripts_dir)
-        flles = [file for file in files if file.endswith(".luisa")]
+        files = [file for file in files if file.endswith(".luisa")]
         file_marks = [file[file.find("_") + 1: file.find(".")] for file in files]
+        idx = [i for i in range(len(file_marks))]
+        idx.sort(key=lambda x: int(file_marks[x]))
         pngs = list()
         for i in range(len(files)):
-            real_mark = f"{args.script_mark}_{file_marks[i]}"
-            result = subprocess.run([
+            real_mark = f"{args.script_mark}_{file_marks[idx[i]]}"
+            command = [
                 args.renderer_path,
                 f"-b CUDA",
                 f"-o \"{args.output_dir}\"",
                 f"-m {real_mark}",
-                f"-r \"{os.path.join(args.script_dir, files[i])}\"",
-            ])
+                f"-r",
+                f"\"{os.path.join(args.scripts_dir, files[idx[i]])}\"",
+            ]
+            print(" ".join(command))
+            result = subprocess.run(command)
             pngs.append(os.path.join(args.output_dir, f"image_{real_mark}.png"))
-        pngs_to_gif(pngs, os.path.join(args.output_dir, f"{args.script_mark}.gif"))
+        gif_file = os.path.join(args.output_dir, f"{args.script_mark}.gif")
+        pngs_to_gif(pngs, gif_file)
+        print(f"Save gif to {gif_file}")
         
     elif args.script_path is not None and args.script_mark is not None:
         result = subprocess.run([
