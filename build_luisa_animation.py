1142c2d6a0b39ba66ce8c2a012facea835eebaee
ACMLCZH <965503066@qq.com>
2023-10-30 02:38:48 +0800

    Update build_luisa_animation.py

--- build_luisa_animation.py
+++ build_luisa_animation.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("--image_scale", type=float, default=1.0)
     parser.add_argument("--render_mp4", action="store_true", default=False)
     parser.add_argument("--no_render", action="store_true", default=False)
+    parser.add_argument("--no_zero", action="store_true", default=False)
     args = parser.parse_args()
 
     if args.scripts_dir is not None and args.script_mark is not None:
@@ -24,7 +25,8 @@ if __name__ == "__main__":
         idx = [i for i in range(len(file_marks))]
         idx.sort(key=lambda x: int(file_marks[x]))
         pngs = list()
-        for i in range(len(files)):
+        mark_start = 1 if args.no_zero else 0
+        for i in range(mark_start, len(file_marks)):
             real_mark = f"{args.script_mark}_{file_marks[idx[i]]}"
             if not args.no_render:
             # if not True:
@@ -38,7 +40,9 @@ if __name__ == "__main__":
                 ]
                 print(" ".join(command))
                 result = subprocess.run(command)
-            pngs.append(os.path.join(args.output_dir, f"image_{real_mark}.png"))
+            real_img = os.path.join(args.output_dir, f"image_{real_mark}.png")
+            if os.path.exists(real_img):
+                pngs.append(real_img)
         imgs = [read_png(png) for png in pngs]
         if args.render_mp4:
             mp4_file = os.path.join(args.output_dir, f"{args.script_mark}.mp4")
